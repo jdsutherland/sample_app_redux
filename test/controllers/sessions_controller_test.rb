ee21adb37899a888b9e5e003deb09149f47415d7
Jeff Sutherland <jdsuther@asu.edu>
2016-12-27 16:32:31 +0700

    Add initial login finding and authentication

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -5,4 +5,14 @@ class SessionsControllerTest < ActionDispatch::IntegrationTest
     get login_path
     assert_response :success
   end
+
+  test "should get create" do
+    post login_path
+    assert_response :success
+  end
+
+  test "should get destroy" do
+    delete logout_path
+    assert_response :success
+  end
 end
