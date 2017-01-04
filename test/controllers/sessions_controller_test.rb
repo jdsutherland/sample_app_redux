06d60ce6f1819d35c2d78e52f7dde66efe3519af
Jeff Sutherland <jdsuther@asu.edu>
2017-01-04 20:29:25 +0700

    Remove unnecessary tests

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -5,15 +5,4 @@ class SessionsControllerTest < ActionDispatch::IntegrationTest
     get login_path
     assert_response :success
   end
-
-  test "should post login" do
-    post login_path, params: { session: { email: '', password: '' } }
-    assert_response :success
-  end
-
-  test "should delete logout" do
-    delete logout_path
-    follow_redirect!
-    assert_response :success
-  end
 end
