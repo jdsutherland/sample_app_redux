d749c04f26a3c514620ce52c18e84570532a52de
Jeff Sutherland <jdsuther@asu.edu>
2017-01-07 13:23:46 +0700

    Add friendly forwarding to redirect indented url

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -25,16 +25,38 @@ class UsersEditTest < ActionDispatch::IntegrationTest
   end
 
   test "successful edit" do
-    name = "Foo Bar"
-    email = "foo@bar.com"
     log_in_as(@user)
     get edit_user_path(@user)
     assert_template 'users/edit'
 
     patch user_path(@user), params: {
       user: {
-        name:  name,
-        email: email,
+        name:  'Foo Bar',
+        email: 'foo@bar.com',
+        password:              '',
+        password_confirmation: '',
+      },
+    }
+
+    assert_not flash.empty?
+    assert_redirected_to @user
+    @user.reload
+    assert_equal name, @user.name
+    assert_equal email, @user.email
+  end
+
+  test "successful edit with friendly forwarding" do
+    get edit_user_path(@user)
+    log_in_as(@user)
+    assert_redirected_to edit_user_url(@user)
+
+    log_in_as(@user)
+    assert_redirected_to @user
+
+    patch user_path(@user), params: {
+      user: {
+        name:  'Foo Bar',
+        email: 'foo@bar.com',
         password:              '',
         password_confirmation: '',
       },
