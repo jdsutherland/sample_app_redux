a0cfd715bc77151053351e5525d53a2e50b9f5a8
Jeff Sutherland <jdsuther@asu.edu>
2017-01-09 15:49:05 +0700

    Fix formatting

--- test/integration/users_index_test.rb
+++ test/integration/users_index_test.rb
@@ -2,8 +2,8 @@ require 'test_helper'
 
 class UsersIndexTest < ActionDispatch::IntegrationTest
   def setup
-    @admin = users(:michael)
-    @non_admin  = users(:archer)
+    @admin     = users(:michael)
+    @non_admin = users(:archer)
   end
 
   test "index including pagination" do
@@ -20,7 +20,7 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
         assert_select 'a[href=?]', user_path(user), text: 'delete'
       end
     end
-    
+
     assert_difference 'User.count', -1 do
       delete user_path(@non_admin)
     end
